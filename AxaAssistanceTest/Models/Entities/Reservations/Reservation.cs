16c46ad7cd8beab148ded0d20f57fd7af706124a
German Bocanegra <german.bocanegrav@gmail.com>
2020-05-20 01:34:36 -0500

    feature: added core entity classes for a library application

---
+++ AxaAssistanceTest/Models/Entities/Reservations/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using AxaAssistanceTest.Models.Entities.Books;
+
+namespace AxaAssistanceTest.Models.Entities.Reservations
+{
+    public class Reservation : AuditEntity
+    {
+        public long Id { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string CustomerId { get; set; }
+        public List<Book> ReservedBooks { get; set; }
+    }
+}
