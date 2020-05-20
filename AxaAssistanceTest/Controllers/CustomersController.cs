f863699993fb415bb6ce21f32375c24538a42329
German Bocanegra <german.bocanegrav@gmail.com>
2020-05-20 14:20:20 -0500

    feature: implemented the CRUD operations in CustomerService

--- AxaAssistanceTest/Controllers/CustomersController.cs
+++ AxaAssistanceTest/Controllers/CustomersController.cs
@@ -1,39 +1,48 @@
-﻿using AxaAssistanceTest.Models.Entities.Customers;
-using System;
+﻿using AxaAssistanceTest.Models.DomainLogic.Service;
+using AxaAssistanceTest.Models.Entities.Customers;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace AxaAssistanceTest.Controllers
 {
     public class CustomersController : ApiController
     {
+        private CustomerService CustomerService;
+
+        public CustomersController() { }
+
+        public CustomersController(CustomerService CustomerService) {
+            this.CustomerService = CustomerService;
+        }
+
         [HttpGet]
         public IEnumerable<Customer> List()
         {
-            return new List<Customer>();
+            return this.CustomerService.ListCustomers();
         }
 
         [HttpGet]
         public Customer Get(string id)
         {
-            return new Customer();
+            return this.CustomerService.GetCustomer(id);
         }
+
         [HttpPost]
         public void Post([FromBody]Customer value)
         {
+            this.CustomerService.SaveCustomer(value);
         }
 
         [HttpPut]
-        public void Put(string id, [FromBody]Customer value)
+        public void Put([FromBody]Customer value)
         {
+            this.CustomerService.UpdateCustomer(value);
         }
 
         [HttpDelete]
         public void Delete(string id)
         {
+            this.CustomerService.DeleteCustomer(id);
         }
     }
 }
--- AxaAssistanceTest/Models/DomainLogic/Service/CustomerService.cs
+++ AxaAssistanceTest/Models/DomainLogic/Service/CustomerService.cs
@@ -1,11 +1,65 @@
-﻿using System;
+﻿using AxaAssistanceTest.Models.Entities.Customers;
+using AxaAssistanceTest.Models.Repositories.Customers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace AxaAssistanceTest.Models.DomainLogic.Service
 {
     public class CustomerService
     {
+        private ICustomerRepository CustomerRepository;
+
+        public CustomerService(ICustomerRepository CustomerRepository)
+        {
+            this.CustomerRepository = CustomerRepository;
+        }
+
+        public List<Customer> ListCustomers()
+        {
+            List<Customer> customers = this.CustomerRepository.GetAllCustomers();
+            customers = customers.OrderBy(c => c.LastName).ToList();
+
+            return customers;
+        }
+
+        public Customer GetCustomer(string id)
+        {
+            Customer customer = this.CustomerRepository.GetCustomer(id);
+
+            if (customer == null)
+            {
+                throw new Exception(string.Format("The resource was not found, Customer with ID: {0}", id));
+            }
+
+            return customer;
+        }
+
+        public void SaveCustomer(Customer value)
+        {
+            value.CreationTime = DateTime.Now;
+            this.CustomerRepository.SaveCustomer(value);
+        }
+
+        public void UpdateCustomer(Customer value)
+        {
+            Customer customer = this.CustomerRepository.GetCustomer(value.Id);
+
+            if (customer != null)
+            {
+                value.CreationTime = customer.CreationTime;
+            }
+            else
+            {
+                value.CreationTime = DateTime.Now;
+            }
+            value.ModificationTime = DateTime.Now;
+            this.CustomerRepository.UpdateCustomer(value);
+        }
+
+        public void DeleteCustomer(string id)
+        {
+            this.CustomerRepository.DeleteCustomer(id);
+        }
     }
 }
