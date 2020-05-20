cc9079656d1322e01b558adddc8572d582418247
German Bocanegra <german.bocanegrav@gmail.com>
2020-05-20 02:54:55 -0500

    feature: added controllers for Crud operations of core entities

---
+++ AxaAssistanceTest/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using AxaAssistanceTest.Models.Entities.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AxaAssistanceTest.Controllers
+{
+    public class BookController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Book> List()
+        {
+            return new List<Book>();
+        }
+
+        [HttpGet]
+        public Book Get(string id)
+        {
+            return new Book();
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Book value)
+        {
+        }
+
+        [HttpPut]
+        public void Put(string id, [FromBody]Book value)
+        {
+        }
+
+        [HttpDelete]
+        public void Delete(string id)
+        {
+        }
+    }
+}
---
+++ AxaAssistanceTest/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using AxaAssistanceTest.Models.Entities.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AxaAssistanceTest.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Customer> List()
+        {
+            return new List<Customer>();
+        }
+
+        [HttpGet]
+        public Customer Get(string id)
+        {
+            return new Customer();
+        }
+        [HttpPost]
+        public void Post([FromBody]Customer value)
+        {
+        }
+
+        [HttpPut]
+        public void Put(string id, [FromBody]Customer value)
+        {
+        }
+
+        [HttpDelete]
+        public void Delete(string id)
+        {
+        }
+    }
+}
